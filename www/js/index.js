10057cbddbb3a7fdfae275b4457b71f85e882c8e
mantasdev1 <32267238+mantasdev1@users.noreply.github.com>
2017-10-09 15:00:18 +0100

    awd
    awd

--- www/js/index.js
+++ www/js/index.js
@@ -7,34 +7,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 	
 function updateDisplay() {
 	
-	// Image when app is launched
-
-	var launched = document.getElementById("#launched");
-	var img = document.createElement("img");
-	img.launched = "https://cdn2.iconfinder.com/data/icons/picons-basic-2/57/basic2-031_hi_bubble_welcome-512.png";
-	launched.appendChild(img);
-	launched.style.height = '50px';
-	launched.style.width = '50px';
-
-	// Image when app is resumed
-
-	var resumed = document.getElementById("#resumed");
-	var img = document.createElement("img");
-	img.resumed = "https://cdn4.iconfinder.com/data/icons/defaulticon/icons/png/256x256/media-play-pause-resume.png";
-	resumed.appendChild(img);
-	resumed.style.height = '50px';
-	resumed.style.width = '50px';
-
-	// Image when app is paused
-
-	var paused = document.getElementById("#paused");
-	var img = document.createElement("img");
-	img.paused = "https://www.almanoticias.com/wp-content/uploads/2016/09/Pause.png";
-	paused.appendChild(img);
-	paused.style.height = '50px';
-	paused.style.width = '50px';
-		
-	
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
 	$("#paused").text("Application resumed: " + resumed_count);
